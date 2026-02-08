3ba4163174f1fc8c7e2f9b21a7c1817c8a4badab
capric98 <42015599+capric98@users.noreply.github.com>
2026-02-08 17:17:50 -0600

    global logger

---
+++ ddmajor/logging.py
@@ -0,0 +1,21 @@
+import logging
+
+
+choices = ["info", "warning", "debug"]
+logger = logging.getLogger("ddmajor")
+
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        fmt="[%(levelname)s][%(asctime)s] %(name)s - %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+)
+
+logger.addHandler(handler)
+
+
+def set_level(level: str | int) -> None:
+    logger.setLevel(logging.getLevelName(level.upper()) if isinstance(level, str) else level)
+
+logger.set_level = set_level
